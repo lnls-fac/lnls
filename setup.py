fda3827fff10e769d2c4bdedd579571ff4d4ade7
ximenes <xresende@gmail.com>
2015-05-26 13:49:10 -0300

    Add version file

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 
 from distutils.core import setup
 
+with open('VERSION', 'r') as _f:
+    __version__ = _f.read().strip()
 
 setup(
     name='LNLS',
-    version='0.1.0',
+    version=__version__,
     description='LNLS utilities',
     url='https://github.com/lnls-fac/lnls',
     packages=['lnls'],
