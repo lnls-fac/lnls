0deb6957875df8d2dc5c4d5aaaa9b072cb69d449
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-05-28 08:53:19 -0300

    Add version information to package

--- lnls/__init__.py
+++ lnls/__init__.py
@@ -1,3 +1,7 @@
+import os as _os
 from lnls.system import *
 from lnls.timer import *
 from . import ids
+
+with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
+    __version__ = _f.read().strip()
--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Topic :: Scientific/Engineering'
     ],
     packages=['lnls'],
+    package_data={'lnls': ['VERSION']},
 
     install_requires=[
         'numpy>=1.8.2',
