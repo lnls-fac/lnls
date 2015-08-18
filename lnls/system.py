9496c13b3af04d5799306e88578acf388e11d937
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-08-18 13:10:41 -0300

    Change excitation curve installation directory

--- lnls/system.py
+++ lnls/system.py
@@ -9,3 +9,5 @@ folder_data = _os.path.join(folder_root, 'data')
 folder_mml  = _os.path.join(folder_code, 'MatlabMiddleLayer', 'Release')
 folder_mml_sirius     = _os.path.join(folder_mml, 'lnls', 'fac_scripts', 'sirius')
 folder_mml_sirius_ids = _os.path.join(folder_mml_sirius, 'insertion_devices')
+
+folder_excitation_curves = _os.path.join(folder_db, 'excitation_curves')
