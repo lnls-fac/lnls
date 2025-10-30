9cc2dd7702e13f7aa0cca7c5ba72bacdf2675111
Ximenes Rocha Resende <xresende@gmail.com>
2025-10-30 09:00:35 -0300

    Remove dialog

--- lnls/__init__.py
+++ lnls/__init__.py
@@ -4,9 +4,9 @@ import os as _os
 
 from lnls.timer import Timer, TimerError
 
-from . import dialog, ids, rotating_coil, utils
+from . import ids, rotating_coil, utils
 
-__all__ = ['utils', 'rotating_coil', 'ids', 'dialog']
+__all__ = ['utils', 'rotating_coil', 'ids']
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
