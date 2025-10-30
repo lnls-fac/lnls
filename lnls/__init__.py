36cf0ec2324ac878f361959f838dcc6e7a511b03
Ximenes Rocha Resende <xresende@gmail.com>
2025-10-29 21:06:59 -0300

    remove notebook module

--- lnls/__init__.py
+++ lnls/__init__.py
@@ -1,11 +1,12 @@
-import os as _os
-from lnls.timer import *
-from . import ids
-from . import utils
-from . import rotating_coil
-from . import notebook
+"""."""
 
-__all__ = ['utils', 'rotating_coil', 'ids', 'dialog','notebook']
+import os as _os
+
+from lnls.timer import Timer, TimerError
+
+from . import dialog, ids, rotating_coil, utils
+
+__all__ = ['utils', 'rotating_coil', 'ids', 'dialog']
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
